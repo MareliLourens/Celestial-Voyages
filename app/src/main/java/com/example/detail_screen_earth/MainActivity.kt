d2be439d9776d48309b7bf4a15ecaf49dae4aeb8
MareliLourens <221119@virtualwindow.co.za>
2024-06-09 16:32:20 +0200

    Source Code

---
+++ app/src/main/java/com/example/detail_screen_earth/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.detail_screen_earth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
